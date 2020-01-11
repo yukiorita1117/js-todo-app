f3ebbe31d6096988773fd99d8a70d41e7e27f99f
折田幸夫 <3204704@gmail.com>
2020-01-12 01:19:57 +0900

    output todo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import { element } from "./view/html-util.js";
+
 export class App {
-  constructor() {
-    console.log("Appを初期化したぞい");
-  }
+  constructor() {}
   //マウントしてくれるメソッドを定義
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
+    const containerElement = document.querySelector("#js-todo-list");
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+    // Todoアイテム数
+    let todoItemCount = 0;
+
+    //submitされた時の動作
     formElement.addEventListener("submit", event => {
       // submitイベントの本来の動作を止めるpreventDefaultメソッド
       event.preventDefault();
-      console.log(`入力欄の値: ${inputElement.value}`);
+
+      // 追加するTodoアイテムの要素(li要素)を作成する
+      const todoElement = element`<li>${inputElement.value}</li>`;
+      // Todoアイテムをcontainerに追加する
+      containerElement.appendChild(todoElement);
+      // Todoアイテム数を+1し、表示されてるテキストを更新する
+      todoItemCount += 1;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      // 入力欄を空文字列にしてリセットする
+      inputElement.value = "";
     });
   }
 }
