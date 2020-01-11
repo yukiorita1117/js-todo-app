20f4ad9fd5611affcae7ad3809c13453b7e1bee6
折田幸夫 <3204704@gmail.com>
2020-01-12 00:31:49 +0900

    Output value of form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-console.log("App.js: loaded");
 export class App {
   constructor() {
     console.log("Appを初期化したぞい");
+  }
+  //マウントしてくれるメソッドを定義
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector("#js-form-input");
+    formElement.addEventListener("submit", event => {
+      // submitイベントの本来の動作を止めるpreventDefaultメソッド
+      event.preventDefault();
+      console.log(`入力欄の値: ${inputElement.value}`);
+    });
   }
 }
