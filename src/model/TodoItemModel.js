f8cbd598dcbef56fbd1ad584e4f83b9b55f2b573
折田幸夫 <3204704@gmail.com>
2020-01-16 00:09:30 +0900

    create TodoItemModel

---
+++ src/model/TodoItemModel.js
@@ -0,0 +1,16 @@
+// ユニークなIDを管理する変数
+let todoIdx = 0;
+
+export class TodoItemModel {
+  /**
+   * @param {string} title Todoアイテムのタイトル
+   * @param {boolean} completed Todoアイテムが完了済みならばtrue、そうでない場合はfalse
+   */
+  constructor({ title, completed }) {
+    // idは自動的に連番となりそれぞれのインスタンス毎に異なるものにしないとあかんよね
+    // つまり呼ばれるItem全て異なるidにしないといけない,なのでインクリメントしているし、letでtodoIdxは定義する
+    this.id = todoIdx++;
+    this.title = title;
+    this.completed = completed;
+  }
+}
