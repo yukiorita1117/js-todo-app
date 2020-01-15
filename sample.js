8991b585d440ee94b7f686bb00cebb2751e2072f
折田幸夫 <3204704@gmail.com>
2020-01-15 23:33:52 +0900

    add sample code

--- sample.js
+++ sample.js
@@ -1,4 +1,42 @@
-import { EventEmitter } from "./src/EventEmitter";
+// import { EventEmitter } from "./src/EventEmitter";
+class EventEmitter {
+  constructor() {
+    // 登録する [イベント名, Set(リスナー関数)] を管理するMap作る
+    this._listeners = new Map();
+  }
+
+  /**
+   * 指定したイベントが実行されたときに呼び出されるリスナー関数を登録するメソッド
+   * @param {string} type イベント名
+   * @param {Function} listener イベントリスナー
+   */
+  addEventListener(type, listener) {
+    // 指定したイベントに対応するSetを作成しリスナー関数を登録する
+    if (!this._listeners.has(type)) {
+      //もしtypeを持ってなかったらSetをnewしてtypeをセットする
+      this._listeners.set(type, new Set());
+    }
+    const listenerSet = this._listeners.get(type);
+    console.log("ここではlistenerはないはず", listenerSet);
+    listenerSet.add(listener);
+  }
+
+  /**
+   * 指定したイベントをディスパッチするメソッド
+   * @param {string} type イベント名
+   */
+  emit(type) {
+    // 指定したイベントに対応するSetを取り出し、すべてのリスナー関数を呼び出す
+    const listenerSet = this._listeners.get(type);
+    if (!listenerSet) {
+      return;
+    }
+    listenerSet.forEach(listener => {
+      listener.call(this);
+    });
+  }
+}
+
 const event = new EventEmitter();
 // イベントリスナー（コールバック関数）を登録
 event.addEventListener("test-event", () => console.log("One!"));
