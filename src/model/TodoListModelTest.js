b6837bca164f4e831eb797b7e73c58edcbba1839
折田幸夫 <3204704@gmail.com>
2020-01-17 01:25:30 +0900

    fix test

--- src/model/TodoListModelTest.js
+++ src/model/TodoListModelTest.js
@@ -95,21 +95,23 @@ class TodoListModel extends EventEmitter {
   }
 }
 
-// 新しいTodoリストを作成する
+//----------------------------------------------------------------
+//新しいTodoリストを作成する
 const todoListModel = new TodoListModel();
-// 現在のTodoアイテム数は0
-console.log(todoListModel.getTotalCount()); // => 0
+console.log("現在のTodoアイテム数は0のはず", todoListModel.getTotalCount());
 // Todoリストが変更されたら呼ばれるイベントリスナーを登録する
 todoListModel.onChange(() => {
-  console.log("TodoListの状態が変わりました");
+  console.log("TodoListが更新されたぞい！");
 });
+
 // 新しいTodoアイテムを追加する
 // => `onChange`で登録したイベントリスナーが呼び出される
 todoListModel.addTodo(
   new TodoItemModel({
-    title: "新しいTodoアイテム",
-    completed: false
+    title: "新しいItem",
+    completed: true
   })
 );
-// Todoリストにアイテムが増える
-console.log(todoListModel.getTotalCount()); // => 1
+
+// Todoリストに追加したアイテムが増える
+console.log("1のはず", todoListModel.getTotalCount());
