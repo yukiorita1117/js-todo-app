045c9db591fc5aa990c4df507088fda035fb8063
折田幸夫 <3204704@gmail.com>
2020-01-18 21:44:31 +0900

    updateTodoの移管

--- src/App.js
+++ src/App.js
@@ -16,33 +16,33 @@ export class App {
 
     //状態が更新されたら呼ばれる
     this.todoListModel.onChange(() => {
-      // 追加するTodoアイテムの要素(li要素)を作成する (elementはview)
-      const todoListElement = element`<ul />`;
-      const todoItems = this.todoListModel.getTodoItems();
+      // // 追加するTodoアイテムの要素(li要素)を作成する (elementはview)
+      // const todoListElement = element`<ul />`;
+      // const todoItems = this.todoListModel.getTodoItems();
       todoItems.forEach(item => {
-        const todoItemElement = item.completed
-          ? element`<li><input type="checkbox" class="checkbox" checked>
-          <s>${item.title}</s>
-          <button class="delete">x</button></input></li>`
-          : element`<li><input type="checkbox" class="checkbox">
-          ${item.title}
-          <button class="delete">x</button></input></li>`;
+        //   const todoItemElement = item.completed
+        //     ? element`<li><input type="checkbox" class="checkbox" checked>
+        //     <s>${item.title}</s>
+        //     <button class="delete">x</button></input></li>`
+        //     : element`<li><input type="checkbox" class="checkbox">
+        //     ${item.title}
+        //     <button class="delete">x</button></input></li>`;
 
         // クラス名checkboxを持つ要素を取得
         const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
         // console.log("todoItemElementの中身", todoItemElement);
         // console.log("inputCheckboxElementの中身", inputCheckboxElement);
 
-        // `<input type="checkbox">`のチェックが変更されたときに呼ばれるイベントリスナーを登録
-        inputCheckboxElement.addEventListener("change", () => {
-          // チェックボックスの表示が変わったタイミングで呼び出される処理
-          // ここでモデルを更新する処理を呼ぶ
-          // 指定したTodoアイテムの完了状態を反転させる → completedの切り替え
-          this.todoListModel.updateTodo({
-            id: item.id,
-            completed: !item.completed
-          });
-        });
+        // // `<input type="checkbox">`のチェックが変更されたときに呼ばれるイベントリスナーを登録
+        // inputCheckboxElement.addEventListener("change", () => {
+        //   // チェックボックスの表示が変わったタイミングで呼び出される処理
+        //   // ここでモデルを更新する処理を呼ぶ
+        //   // 指定したTodoアイテムの完了状態を反転させる → completedの切り替え
+        //   this.todoListModel.updateTodo({
+        //     id: item.id,
+        //     completed: !item.completed
+        //   });
+        // });
 
         // 削除ボタン(x)をクリック時にTodoListModelからアイテムを削除する
         const deleteButtonElement = todoItemElement.querySelector(".delete");
