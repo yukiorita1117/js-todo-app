f93cc487431eceaa5499d02845f1bc5bd55bb6ba
折田幸夫 <3204704@gmail.com>
2020-01-29 00:12:08 +0900

    add conditionalBranch

--- learn/conditionalBranch.js
+++ learn/conditionalBranch.js
@@ -4,15 +4,15 @@ const kg = prompt(`体重をを入力してください`);
 function Bmi(cm, kg) {
   const shincho = cm / 100;
   const bmiNum = Math.round((kg / shincho) * shincho);
-  if (bmiNum > 27) {
+  if (bmiNum >= 27) {
     return "かなり太りすぎ";
-  } else if (bmiNum > 23 && bmiNum < 26) {
+  } else if (bmiNum >= 23 && bmiNum <= 26) {
     return "ちょっと太り気味";
-  } else if (bmiNum > 21 && bmiNum < 22) {
+  } else if (bmiNum >= 21 && bmiNum <= 22) {
     return "普通";
-  } else if (bmiNum > 18 && bmiNum < 20) {
+  } else if (bmiNum >= 18 && bmiNum <= 20) {
     return "ちょっと痩せ気味";
-  } else if (bmiNum < 17) {
+  } else if (bmiNum <= 17) {
     return "痩せ気味";
   }
 }
